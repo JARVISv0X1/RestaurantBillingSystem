77e0d6e10c301886f27c51ea8c8e1b55987bd415
Karan Khallar <karan.khallar.courses@gmail.com>
2024-02-01 00:01:13 +0530

    Ui Fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BillingForm from "./components/BillingForm";
 function App() {
   return (
     <>
-      <div className="mx-auto">
+      <div className="mx-auto container">
         <h1 className="mx-auto mt-1 text-center">Resturant Billing system</h1>
         <BillingForm className="mt-5 table"></BillingForm>
         
